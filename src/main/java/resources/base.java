0ebc9a231de20fe7525b8be330853c02652b89be
timlee <wentin.lee@gmail.com>
2019-12-31 15:04:45 +0800

    Update base.java

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -22,7 +22,7 @@ public WebDriver initializeDriver() throws IOException
 {
 	
  prop= new Properties();
-FileInputStream fis=new FileInputStream(".//data.properties");
+FileInputStream fis=new FileInputStream("..//resources//data.properties");
 
 prop.load(fis);
 String browserName=prop.getProperty("browser");
